29f018d003e63d5e373591edbc98f56117a3d0f1
egbejob680 <jobegbe39@gmail.com>
2022-11-05 20:10:04 +0100

    updated

---
+++ PHP/first.php
@@ -0,0 +1,15 @@
+
+<?php
+/*object oriented programing */
+
+class fruit{
+public $name;
+public $color;
+function set_name($name){
+    $this->name = $name;
+}
+}
+$apple = new fruit();
+$apple->set_name = "apple"
+
+?>
